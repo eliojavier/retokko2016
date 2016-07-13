ba098d9f64aee5288c180585a9e8d1649ec9afdf
Elio Acosta <eliojavier86@gmail.com>
2016-07-13 18:13:03 -0430

    commit

--- resources/views/site/clasificados.blade.php
+++ resources/views/site/clasificados.blade.php
@@ -1,11 +1,17 @@
 @extends('app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8">
-            <img class="img-responsive" src="asset{'/img/clasificados.jpg'}" alt="">
+
+    <div class="container-fluid">
+        <div class="row strip">
+
         </div>
     </div>
-</div>
+    <div class="container">
+        <div class="row margin-top-20">
+            <div class="col-md-8 col-md-offset-2">
+                <img class="img-responsive" src="{{asset('/img/clasificados.jpg')}}" alt="">
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/site/ganadores.blade.php
+++ resources/views/site/ganadores.blade.php
@@ -2,10 +2,20 @@
 
 @section('content')
 
+    <div class="container-fluid">
+        <div class="row strip">
+
+        </div>
+    </div>
     <div class="container">
-        GANADORES 2015
+        <div class="row margin-top-20">
+            <div class="col-md-8 col-md-offset-2">
+                <img class="img-responsive" src="{{asset('/img/ganadores.jpg')}}" alt="">
+            </div>
+        </div>
     </div>
 
+    <div class="margin-top-20"></div>
     @include('site.secciones.twitter_seccion')
 
     @include('site.secciones.youtube_iframe')
